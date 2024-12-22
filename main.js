09c42267ff7af3edb1924a9f8169562d4c4b8e5f
Upmanyu-B <139064550+Upmanyu-B@users.noreply.github.com>
2024-12-22 09:51:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() {
+    canvas = creatCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 620, 410);
+}
